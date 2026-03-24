a10519cf1a46c7c89ea661c4a5ac129e18515c67
mateivul <mateivultur08@gmail.com>
2026-03-24 15:50:35 +0200

    presets

---
+++ src/data/presets.js
@@ -0,0 +1,32 @@
+export const presets = {
+    vacation: {
+        name: "Vacation",
+        suggestedStats: ["schoolDays", "weekends", "weeks", "hours", "funSleep"],
+        defaultMessage: "Vacation!!!!",
+        defaultAccent: "#ff6b35",
+    },
+    exam: {
+        name: "Exam",
+        suggestedStats: ["schoolDays", "weekends", "hours", "minutes", "workdayBar"],
+        defaultMessage: "Good luck!!",
+        defaultAccent: "#b0229f",
+    },
+    birthday: {
+        name: "Birthday",
+        suggestedStats: ["weeks", "hours", "minutes", "funSleep", "funMeals"],
+        defaultMessage: "Happy Birthday!",
+        defaultAccent: "#e040fb",
+    },
+    newYear: {
+        name: "New Year",
+        suggestedStats: ["weeks", "hours", "minutes", "funMeals"],
+        defaultMessage: "Happy New Year!!",
+        defaultAccent: "#ff7203",
+    },
+    custom: {
+        name: "Custom",
+        suggestedStats: ["hours", "minutes"],
+        defaultMessage: "",
+        defaultAccent: "#33e81f",
+    },
+};
